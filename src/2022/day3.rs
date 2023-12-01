17fb72d1e5679061cd33368f013e2dd1d3e2997b
Jonathanshrek <jonathanshrek@gmail.com>
2023-11-30 21:30:29 -0500

    new year prep

---
+++ src/2022/day3.rs
@@ -0,0 +1,12 @@
+#[aoc(day3, part1)]
+pub fn solve_part1(_input: &str) -> i32 {
+  // let characters: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  // let characters_string = characters.to_string();
+
+  // for i in 0..characters_string.len() {
+  //   println!("{}", characters_string.chars().nth(i).unwrap());
+  // }
+
+  4
+}
